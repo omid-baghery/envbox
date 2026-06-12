23f7ebef4192e97426f157c91be6aadeb4a5bd6e
Omid Bagheri <omid.b193@gmail.com>
2026-06-12 15:02:34 +0430

    create login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/shared/components/ui/card";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/shared/components/ui/tabs";
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <Tabs defaultValue="signin" className="mx-auto w-full my-6 py-6 px-4">
+      <TabsList>
+        <TabsTrigger value="signin">Sign In</TabsTrigger>
+        <TabsTrigger value="signup">Sign up</TabsTrigger>
+      </TabsList>
+      <Card>
+        <TabsContent value="signin">
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold">Sign in</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>signin</p>
+          </CardContent>
+        </TabsContent>
+
+        <TabsContent value="signup">
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold">Sign up</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>signup</p>
+          </CardContent>
+        </TabsContent>
+      </Card>
+    </Tabs>
+  );
+}
