8ae18ff3b23f2ca4acabac0fadfaa442fa48f975
Omid Bagheri <omid.b193@gmail.com>
2026-06-17 23:16:17 +0430

    create members.actions p1

---
+++ src/features/members/members.actions.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import { db } from "@/shared/db";
+import { requireProjectOwner } from "../projects/authorization";
+import { environments, inviteTokens, projectMembers } from "@/shared/db/schema";
+import { and, eq, inArray } from "drizzle-orm";
+import { randomUUID } from "crypto";
+
+const EXPIRY_MS: Record<string, number> = {
+  "1h": 60 * 60 * 1000,
+  "6h": 6 * 60 * 60 * 1000,
+  "24h": 24 * 60 * 60 * 1000,
+};
+
+export async function inviteMember(input: {
+  projectId: string;
+  environmentIds: string[];
+  expiry: "1h" | "6h" | "24h";
+  inviteEmail?: string;
+}) {
+  await requireProjectOwner(input.projectId);
+
+  if (input.environmentIds.length === 0) {
+    throw new Error("Select at least one environment");
+  }
+
+  // چک کردن معتبر بودن ولیدیشن ها
+  const validEnvs = await db
+    .select({ id: environments.id })
+    .from(environments)
+    .where(
+      and(
+        eq(environments.projectId, input.projectId),
+        inArray(environments.id, input.environmentIds),
+      ),
+    );
+
+  if (validEnvs.length !== input.environmentIds.length) {
+    throw new Error("Invalid environment selection");
+  }
+
+  // ============ create new member
+  const memberId = randomUUID();
+
+  await db.insert(projectMembers).values({
+    id: memberId,
+    projectId: input.projectId,
+    inviteEmail: input.inviteEmail ?? null,
+    role: "member",
+    environmentIds: input.environmentIds,
+    status: "pending",
+  });
+
+  // ============= create token
+  const rawToken = "placeholder-token";
+  const tokenHash = "placeholder-hash";
+
+  await db.insert(inviteTokens).values({
+    id: randomUUID(),
+    projectId: input.projectId,
+    memberId,
+    tokenHash,
+    expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000), // ۲۴ ساعت
+  });
+
+  return { command: `npx envbox join ${rawToken}` };
+}
