f3f204ab4032605f744a1be78e6e01ba300ef12e
Omid Bagheri <omid.b193@gmail.com>
2026-06-20 22:16:37 +0430

    email verification p3 - set emilVerifictaion tab in login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -16,9 +16,15 @@ import SignInTab from "./_components/sign-in-tab";
 import SignUpTab from "./_components/sign-up-tab";
 import { authClient } from "@/features/auth/auth-client";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+type Tab = "signin" | "signup" | "email-verification" | "forgot-password";
 
 export default function LoginPage() {
   const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [selectedTab, setSelectedTab] = useState<Tab>("signin");
+
   const { data: session, isPending } = authClient.useSession();
 
   // Redirect if already logged in
@@ -32,7 +38,12 @@ export default function LoginPage() {
 
   return (
     <div className="flex min-h-screen items-center justify-center px-4">
-      <Tabs defaultValue="signin" className="w-full max-w-sm">
+      <Tabs
+        value={selectedTab}
+        onValueChange={(t) => setSelectedTab(t as Tab)}
+        defaultValue="signin"
+        className="w-full max-w-sm"
+      >
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="signin">Sign In</TabsTrigger>
           <TabsTrigger value="signup">Sign Up</TabsTrigger>
@@ -56,6 +67,17 @@ export default function LoginPage() {
             </CardHeader>
             <CardContent>
               <SignUpTab />
+            </CardContent>
+          </Card>
+        </TabsContent>
+
+        <TabsContent value="email-verification">
+          <Card>
+            <CardHeader className="text-2xl font-bold">
+              <CardTitle>Verify Your Email</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <EmailVerification email={email} />
             </CardContent>
           </Card>
         </TabsContent>
