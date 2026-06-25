c1115f6fd3ec75b080f26de0cafb1141898f6b53
Omid Bagheri <omid.b193@gmail.com>
2026-06-25 16:23:30 +0430

    complete social-auth-buttons.tsx

--- src/app/(auth)/login/_components/social-auth-buttons.tsx
+++ src/app/(auth)/login/_components/social-auth-buttons.tsx
@@ -1,5 +1,35 @@
 "use client";
 
+import { authClient } from "@/features/auth/auth-client";
+import {
+  SUPPORTED_OAUTH_PROVIDER_DETAILS,
+  SUPPORTED_OAUTH_PROVIDERS,
+} from "@/features/auth/o-auth-providers";
+import { Button } from "@/shared/components/ui/button";
+
 export function SocialAuthButtons() {
-  return <div>fsd</div>;
+  return (
+    <div className="grid grid-cols-2 gap-3 w-full">
+      {SUPPORTED_OAUTH_PROVIDERS.map((provider) => {
+        const { name, Icon } = SUPPORTED_OAUTH_PROVIDER_DETAILS[provider];
+
+        return (
+          <Button
+            key={provider}
+            variant="outline"
+            className="w-full"
+            onClick={() =>
+              authClient.signIn.social({
+                provider,
+                callbackURL: "/dashboard",
+              })
+            }
+          >
+            <Icon />
+            {name}
+          </Button>
+        );
+      })}
+    </div>
+  );
 }
