51365aa614d1bbf9e0171a9545fcaca369f23ab3
Omid Bagheri <omid.b193@gmail.com>
2026-06-26 13:04:18 +0430

    change proxy for page protection

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,19 +3,28 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export default async function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
   const session = await auth.api.getSession({
     headers: request.headers,
   });
 
+  if (
+    session &&
+    (pathname === "/login" ||
+      pathname === "/register" ||
+      pathname === "/verify-email")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
   // لاگین نیست → برو login
-  if (!session) {
-    const loginUrl = new URL("/login", request.url);
-    return NextResponse.redirect(loginUrl);
+  if (!session && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/login", "/register", "/verify-email"],
 };
